31cbe8f71522d41b80ecff14696637df1daad11c
William Stephens <william.a.stephens17@gmail.com>
2020-01-22 16:52:06 -0600

    add movie function is working

--- src/api.js
+++ src/api.js
@@ -2,5 +2,18 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+  addMovie: (title, rating, id) => {
+    const moviePost = {title: title, rating: rating, id: id};
+    const url = '/api/movies';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(moviePost),
+    };
+    fetch(url, options)
+        .then(response => response.json())
   }
 };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
  * require style imports
  */
 const $ = require('jquery');
-const {getMovies} = require('./api.js');
+const {getMovies, addMovie} = require('./api.js');
 const container = $('#container');
+
+const submitNewMovie = $('#add-movie-button');
+const addMovieName = $('#add-movie-name');
+const addMovieRating = $('#add-movie-rating');
+const addMovieId = $('#add-movie-id');
+submitNewMovie.click(function(){
+  addMovie(addMovieName.val(), addMovieRating.val(), addMovieId.val());
+});
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
