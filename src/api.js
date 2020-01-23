4b810d36ebd56df8bb844204dba896ce13fbc66e
William Stephens <william.a.stephens17@gmail.com>
2020-01-23 09:33:08 -0600

    Updated ID so it's generated automatically.

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
+let movieID = 4;
+
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
   },
-  addMovie: (title, rating, id) => {
-    const moviePost = {title: title, rating: rating, id: id};
+  addMovie: (title, rating) => {
+    const moviePost = {title: title, rating: rating, id: movieID};
+    movieID++;
     const url = '/api/movies';
     const options = {
       method: 'POST',
