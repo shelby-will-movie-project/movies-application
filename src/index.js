00fbf93acd60a8f71ec7660688124b2ed6cd2f7d
William Stephens <william.a.stephens17@gmail.com>
2020-01-24 11:45:09 -0600

    Cleaned up code in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,54 @@ const $ = require('jquery');
 //Node import from api.js
 const {getMovies, addMovie, editMovie, deleteMovie} = require('./api.js');
 
+//Main content area containers
 const container = $('#container');
 const loading = $('#loading');
+
+//Reloads content areas for all CRUD operations
+function refreshMovies() {
+  getMovies().then((movies) => {
+    let result = '';
+    movies.forEach(({title, rating, id, img}) => {
+      if(img === "") {
+        img = "img/Coming-Soon.jpg";
+      }
+      result +=
+          "<div class=\"flip-card\">" +
+          "<div class=\"flip-card-inner\">" +
+          "<div class=\"flip-card-front\">"+
+          "<img src=\"" + img +"\" alt=\"Avatar\" style=\"width:300px;height:300px;\">" +
+          "</div>" +
+          "<div class=\"flip-card-back\">" +
+          "<h1>Title: " + title + "</h1>" +
+          "<p>Rating: " + rating + "</p>" +
+          "<p>ID: " + id + "</p>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+    });
+    loading.hide();
+    container.show();
+    container.html(result);
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
+  });
+}
+
+//Add movie
+const submitNewMovie = $('#add-movie-button');
+const addMovieName = $('#add-movie-name');
+const addMovieRating = $('#add-movie-rating');
+submitNewMovie.click(function(){
+  container.hide();
+  loading.show();
+  loading.html("<div class='loadingio-spinner-spinner-ib6rz94n49a loading1'><div class='ldio-ioww6g0mvq loading2'>" +
+      "<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>" +
+      "<div></div><div></div></div></div>");
+  addMovie(addMovieName.val(), addMovieRating.val());
+  refreshMovies();
+});
 
 //Edit movie
 const editMovieButton = $("#edit-movie-button");
@@ -18,74 +64,10 @@ editMovieButton.click(function(){
       "<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>" +
       "<div></div><div></div></div></div>");
   editMovie(editMovieID.val(), editMovieName.val(), editMovieRating.val());
-  getMovies().then((movies) => {
-    let result = '';
-    movies.forEach(({title, rating, id, img}) => {
-      if(img === "") {
-        img = "img/Coming-Soon.jpg";
-      }
-      result +=
-          "<div class=\"flip-card\">" +
-          "<div class=\"flip-card-inner\">" +
-          "<div class=\"flip-card-front\">"+
-          "<img src=\"" + img +"\" alt=\"Avatar\" style=\"width:300px;height:300px;\">" +
-          "</div>" +
-          "<div class=\"flip-card-back\">" +
-          "<h1>Title: " + title + "</h1>" +
-          "<p>Rating: " + rating + "</p>" +
-          "<p>ID: " + id + "</p>" +
-          "</div>" +
-          "</div>" +
-          "</div>";
-    });
-    loading.hide();
-    container.show();
-    container.html(result);
-  }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
-  });
+  refreshMovies();
 });
 
-const submitNewMovie = $('#add-movie-button');
-const addMovieName = $('#add-movie-name');
-const addMovieRating = $('#add-movie-rating');
-submitNewMovie.click(function(){
-  container.hide();
-  loading.show();
-  loading.html("<div class='loadingio-spinner-spinner-ib6rz94n49a loading1'><div class='ldio-ioww6g0mvq loading2'>" +
-      "<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>" +
-      "<div></div><div></div></div></div>");
-  addMovie(addMovieName.val(), addMovieRating.val());
-  getMovies().then((movies) => {
-    let result = '';
-    movies.forEach(({title, rating, id, img}) => {
-      if(img === "") {
-        img = "img/Coming-Soon.jpg";
-      }
-      result +=
-          "<div class=\"flip-card\">" +
-          "<div class=\"flip-card-inner\">" +
-          "<div class=\"flip-card-front\">"+
-          "<img src=\"" + img +"\" alt=\"Avatar\" style=\"width:300px;height:300px;\">" +
-          "</div>" +
-          "<div class=\"flip-card-back\">" +
-          "<h1>Title: " + title + "</h1>" +
-          "<p>Rating: " + rating + "</p>" +
-          "<p>ID: " + id + "</p>" +
-          "</div>" +
-          "</div>" +
-          "</div>";
-    });
-    loading.hide();
-    container.show();
-    container.html(result);
-  }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
-  });
-});
-
+//Delete movie
 const deleteMovieID = $("#delete-movie-id");
 const deleteMovieButton = $("#delete-movie-button");
 deleteMovieButton.click(function(){
@@ -95,35 +77,10 @@ deleteMovieButton.click(function(){
       "<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>" +
       "<div></div><div></div></div></div>");
   deleteMovie(deleteMovieID.val());
-  getMovies().then((movies) => {
-    let result = '';
-    movies.forEach(({title, rating, id, img}) => {
-      if(img === "") {
-        img = "img/Coming-Soon.jpg";
-      }
-      result +=
-          "<div class=\"flip-card\">" +
-          "<div class=\"flip-card-inner\">" +
-          "<div class=\"flip-card-front\">"+
-          "<img src=\"" + img +"\" alt=\"Avatar\" style=\"width:300px;height:300px;\">" +
-          "</div>" +
-          "<div class=\"flip-card-back\">" +
-          "<h1>Title: " + title + "</h1>" +
-          "<p>Rating: " + rating + "</p>" +
-          "<p>ID: " + id + "</p>" +
-          "</div>" +
-          "</div>" +
-          "</div>";
-    });
-    loading.hide();
-    container.show();
-    container.html(result);
-  }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
-  });
+  refreshMovies();
 });
 
+//Initial page load
 getMovies().then((movies) => {
   container.hide();
   let result = '';
